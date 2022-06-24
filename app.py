da6a2b5540c823edeeb2518d68f220ee136a92ab
hlf20010508 <hlf01@icloud.com>
2022-06-24 23:55:53 +0800

    实现上传文件和发送消息的功能

--- app.py
+++ app.py
@@ -1,28 +1,60 @@
-import OSS_minio
 import flask
+import time
+import OSS_minio
 import mysql_db
+import config as myconfig
+import os
 
-# db=mysql_db.db()
+config = myconfig.load()
 
-client=OSS_minio.init()
+db, table = mysql_db.db()
+
+client = OSS_minio.init()
+
+host_minio = os.path.join(config['host_minio'], config['bucket'])
 
 app = flask.Flask(__name__)
 
-@app.route('/', methods=['POST', 'GET'])
+
+def query_items(start, amount):
+    return db.query('select * from %s order by time desc LIMIT %d, %d' % (table, start, amount))
+
+
+def push_item(item):
+    db.table_insert(table, item)
+
+
+@app.route('/', methods=['GET'])
 def index():
-    if flask.request.method == 'POST':
-        f = flask.request.files.get('file')
-        if f:
-            print('uploading ...')
-            client.upload_stream(f.filename,f) #流式上传
-            return flask.redirect(flask.url_for('index'))
-        else:
-            return flask.redirect(flask.url_for('download_stream'))
     return flask.render_template('index.html')
 
-@app.route('/download')
-def download_stream():
-    file_name='app.py'
-    print('downloading ...')
-    file_stream=client.download_stream(file_name) #流式下载
-    return flask.send_file(file_stream,download_name=file_name,as_attachment=True)
+
+@app.route('/get/page', methods=['GET'])
+def page():
+    item_per_page = config['item_per_page']
+    page_id = int(flask.request.args['page'])
+    start = page_id*item_per_page
+    result = query_items(start, item_per_page)
+    return flask.jsonify({'messages': result})
+
+
+@app.route('/post/files', methods=['POST'])
+def upload():
+    f = flask.request.files.get('file')
+    print('uploading ...')
+    client.upload_stream(f.filename, f)  # 流式上传
+
+    time_parse = int(round(time.time() * 1000))
+    return {
+        "time": time_parse,
+        "url": os.path.join(host_minio, f.filename)
+    }
+
+
+@app.route('/post/message', methods=['POST'])
+def send():
+    item = flask.request.get_json(silent=True)
+    push_item(item)
+    return {
+        "sucess": True
+    }
--- config.py
+++ config.py
@@ -1,14 +1,47 @@
 import json
-host = input('请输入mysql服务器域名或ip地址：')
-user = input('请输入用户名：')
-password = input('请输入密码：')
-database = input('请输入数据库名：')
-config = {
-    'host': host,
-    'user': user,
-    'password': password,
-    'database': database,
-}
-config_file = open('config.json', 'w')
-json.dump(config, config_file)
-print('设置成功！')
+
+
+def init():
+    host_minio = input('请输入minio服务器完整域名或ip地址 (eg: http://example.com:9000)：')
+    user_minio = input('请输入用户名：')
+    password_minio = input('请输入密码：')
+    bucket = input('请输入bucket名：')
+    host_mysql = input(
+        '请输入mysql服务器域名或ip地址，必须使用默认端口 (eg: example.com) ：')
+    user_mysql = input('请输入用户名：')
+    password_mysql = input('请输入密码：')
+    database = input('请输入数据库名：')
+    table = input('请输入表名：')
+    item_per_page = input('请输入每次加载的项目条数：')
+
+    config = {
+        'host_minio': host_minio,
+        'user_minio': user_minio,
+        'password_minio': password_minio,
+        'bucket': bucket,
+        'host_mysql': host_mysql,
+        'user_mysql': user_mysql,
+        'password_mysql': password_mysql,
+        'database': database,
+        'table': table,
+        'item_per_page': int(item_per_page)
+    }
+    config_file = open('config.json', 'w')
+    json.dump(config, config_file)
+    print('设置成功！')
+
+
+def load():
+    try:
+        config_file = open('config.json', 'r')
+    except:
+        print('未找到数据库配置文件，请先运行config.py')
+        print('python config.py')
+        exit()
+    config = json.load(config_file)
+    config_file.close()
+    return config
+
+
+if __name__ == '__main__':
+    init()
