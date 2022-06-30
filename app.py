05f2b5cf50392fedc5f9e93520dd6fe272a0f478
hlf20010508 <hlf01@icloud.com>
2022-06-30 21:16:08 +0800

    socketio 支持跨域

--- app.py
+++ app.py
@@ -13,7 +13,7 @@ table = config['table']
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'secret!'
-socketio = SocketIO(app)
+socketio = SocketIO(app,cors_allowed_origins='*')
 
 
 def query_items(start, amount):
